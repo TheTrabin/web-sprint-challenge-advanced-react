ba23f045a78c49bb93b99e5a3eb21c62fb8c6696
TheTrabin <beyondtears@gmail.com>
2020-05-17 19:24:03 -0700

    MVP including Testing complete. All Tests pass with verification using Await to clear if SuccessMessage is found, and it is infact found upon submit.

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -7,7 +7,7 @@ import { useForm } from "../hooks/useForm";
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues, handleChanges] = useForm();
+  const [values, handleChanges] = useForm();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,6 +24,7 @@ const CheckoutForm = (props) => {
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
+            data-testid="firstName"
           />
         </label>
         <label>
@@ -32,6 +33,7 @@ const CheckoutForm = (props) => {
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
+            data-testid="lastName"
           />
         </label>
         <label>
@@ -40,21 +42,22 @@ const CheckoutForm = (props) => {
             name="address"
             value={values.address}
             onChange={handleChanges}
+            data-testid="address"
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={values.city} onChange={handleChanges} data-testid="city"/>
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={values.state} onChange={handleChanges} data-testid="state"/>
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={values.zip} onChange={handleChanges} data-testid="zip"/>
         </label>
-        <button>Checkout</button>
+        <button data-testid="sub">Checkout</button>
       </form>
 
       {showSuccessMessage && (
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByTestId, findByTestId } = render(<CheckoutForm />);
+}); //pass
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    const { getByTestId, findByTestId } = render(<CheckoutForm />); //pass
+
+    const firstName = getByTestId("firstName"); //pass
+    const lastName = getByTestId("lastName"); //pass
+    const address = getByTestId("address"); //pass
+    const city = getByTestId("city"); //pass
+    const state = getByTestId("state"); //pass
+    const zip = getByTestId("zip"); //pass
+    const sub = getByTestId("sub"); //pass
+
+    
+
+    const fName = "Mike";
+    const lName = "McLeod";
+    const addy = "1010 digital alley way";
+    const cty = "Tacoma";
+    const ste = "WA";
+    const zipc = "10101";
+
+    firstName.innerHTML = fName;
+    lastName.innerHTML = lName;
+    address.innerHTML = addy;
+    city.innerHTML = cty;
+    state.innerHTML = ste;
+    zip.innerHTML = zipc;
+
+    fireEvent.click(sub); //pass
+
+    const success = await getByTestId("successMessage"); //pass
+});
